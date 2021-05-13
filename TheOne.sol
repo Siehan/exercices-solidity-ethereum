0cac6a3f49d661f244d84825a87ee53e1ce6615f
Siehan <memain.ye.sylvie@gmail.com>
2021-05-13 11:49:08 +0200

    done & understood

--- TheOne.sol
+++ TheOne.sol
@@ -1,10 +1,3 @@
-// 0x58d261dbaDfFfF47cf4481A0553caDF13529F223
-
-/*
-Ecrivez un contrat TheOne.
-Ce contrat devra posséder une fonction one qui retournera le nombre 1 lorsqu'elle sera appelée.
-*/
-
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
