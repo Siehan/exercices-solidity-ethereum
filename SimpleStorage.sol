deb8056b5dc1a606910c662f60e161f6a094e29d
Siehan <memain.ye.sylvie@gmail.com>
2021-05-13 11:48:00 +0200

    done en understood

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,11 +1,3 @@
-/// Contrat 0xE3aDEaf69b2C40C5004bb675fB14733E15DdF995
-
-/*
-Ecrivez un contrat SimpleStorage qui modifiera/affichera le contenu d'une variable uint256 private _storedData.
-Une fonction storedData() retournera la valeur de _storedData.
-Une fonction setStoredData(uint256 value) modifiera la valeur de _storedData par la value passé en paramètre.
-*/
-
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
